b671dbec94b816f2e29846dc6350902c5adc81bd
MrPesh4k <82715095+MrPesh4k@users.noreply.github.com>
2021-04-21 16:24:30 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = ' [ᕦ༼◣_◢༽つ`JBC SITE~] ';
 
-myVariable = 'SUCC';
+document.querySelector('h1').onclick = function() {
+    alert('НЕ ТРОГАЙ МЕНЯ');
+}
