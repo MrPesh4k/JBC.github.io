22ebcabc584d73a6b8f4fe417822d3cd253ef6f8
MrPesh4k <82715095+MrPesh4k@users.noreply.github.com>
2022-01-12 14:03:18 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = ' [ᕦ༼◣_◢༽つ`JBC SITE~] ';
+myHeading.textContent = ' \VTV… ';
 
 document.querySelector('h1').onclick = function() {
     alert('НЕ ТРОГАЙ МЕНЯ');
